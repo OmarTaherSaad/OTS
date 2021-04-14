aa4995da31a3ff7fe012594483c35d18ea62e425
Omar Taher Saad <Omartahersaad@outlook.com>
2021-04-14 06:00:25 -0400

    Allow Telescope on production & enable its night mode.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // Make Telescope available locally only
-        if ($this->app->isLocal()) {
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+        $this->app->register(TelescopeServiceProvider::class);
     }
 
     /**
