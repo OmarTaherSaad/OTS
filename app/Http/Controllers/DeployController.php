652ba58e8d340a72551d407333f255b97a1e55ab
Omar Taher Saad <omartahersaad@outlook.com>
2021-06-17 03:01:28 +0200

    Update DeployController.php

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Symfony\Component\Process\Process;
 
 class DeployController extends Controller
