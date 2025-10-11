b1ed9df8d2a5be2df074892e5eccd34a2da36891
Omar Taher Saad <omartahersaad@outlook.com>
2025-10-11 19:56:29 +0300

    feat: update language files and localization
    - Update Arabic language file (lang/ar/main.php)
    - Update English language file (lang/en/main.php)
    - Ensure proper localization support for Laravel 12

--- lang/en/main.php
+++ lang/en/main.php
@@ -5,6 +5,6 @@ return [
     'copyrights' => 'Copyrights reserved | Omar Taher Saad '. html_entity_decode('&copy;') ." ". now()->year,
 
 
-    "youtuberLong" => 'YouTuber @ <span class="text-danger"><i class="fab fa-youtube"></i> OTS Channel</span>',
+    "youtuberLong" => 'YouTuber @ <span class="text-danger"><i class="icon icon-youtube"></i> OTS Channel</span>',
 
 ];
